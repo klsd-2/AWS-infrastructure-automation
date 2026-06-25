208f0e7e616ae11ab9ac8e8a1e3d9a5c9cf0becc
KLSD <gudikandulav49@gmail.com>
2026-06-25 19:06:22 +0530

    Terraform variables for AWS configuration

---
+++ terraform/variables.tf
@@ -0,0 +1,15 @@
+variable "aws_region" {
+  default = "ap-south-1"
+}
+
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
+
+variable "cluster_name" {
+  default = "demo-eks-cluster"
+}
+
+variable "db_name" {
+  default = "appdb"
+}
